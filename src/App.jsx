b6cc62ca1f261cc07955f91ade854526eca36bd0
CPC2-2 <cmtorreno@gmail.com>
2025-08-07 17:14:33 +0200

    updateBoard and turns mechanics

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,36 @@ const TURNS = {
   O: 'o'
 }
 
-const Square = ({children,update,index}) => {
+const Square = ({children, isSelected ,updateBoard,index}) => {
+
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+
+  const handleClick = () => {
+    updateBoard();
+  }
+
   return(
-    <div className='square'>
+    <div onClick={updateBoard} className={className}>
       {children}
     </div>
   )
 }
 
-const board = new Array(9).fill(null);
 
 export default function App() {
+
+  const [board,setBoard] = useState(Array(9).fill(null));
+
+  const [turn,setTurn] = useState(TURNS.X);
+
+  //funcction to update the board
+  const updateBoard = () => {
+    //check what turn it is X or O
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  }
+
+
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
@@ -28,13 +47,23 @@ export default function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
-                {index}
+                {board[index]}
               </Square>
             )
           })
         }
 
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
