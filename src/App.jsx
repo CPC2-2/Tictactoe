c301edff904539e4632c3dfeba6598477b869c29
CPC2-2 <cmtorreno@gmail.com>
2025-08-07 17:47:00 +0200

    winner mechanics , winner combos and game stop playing mechanics

--- src/App.jsx
+++ src/App.jsx
@@ -21,23 +21,56 @@ const Square = ({children, isSelected ,updateBoard,index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 export default function App() {
 
   const [board,setBoard] = useState(Array(9).fill(null));
-
   const [turn,setTurn] = useState(TURNS.X);
+  const [winner,setWinner] = useState(null); //null no winner and false is draw
+
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a,b,c] = combo;
+      if(
+        boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c] // 2
+      ) {
+        return boardToCheck[a]; //return the winner
+      }
+    }
+    return null; //no winner
+  }
 
   //function to update the board
   const updateBoard = (index) => {
 
-    const newBoard = [...board];
-    newBoard[index] = turn;
-    setBoard(newBoard);
+    if (board[index] || board.includes(null) === false || winner) {
+      //if the square is already filled or there are no empty squares, do nothing
+      return;
+    }else{
+      const newBoard = [...board];
+      newBoard[index] = turn;
+      setBoard(newBoard);
+  
+      //check what turn it is X or O
+      const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+      setTurn(newTurn);
 
-    //check what turn it is X or O
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
-    setTurn(newTurn);
+      const newWinner = checkWinner(newBoard);
+      if (newWinner) {
+        setWinner(newWinner);
+      }
+    }
   }
 
 
